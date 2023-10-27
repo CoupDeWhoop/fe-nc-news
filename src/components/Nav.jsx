e97db59c318331601471257851b6b96a3cc19585
CoupDeWhoop <greenland.david@gmail.com>
2023-10-27 13:33:08 +0100

    fixed bug in sort asc desc

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import { Link, useSearchParams } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import { getTopics } from '../../utils/api';
+import SortPanel from './SortPanel';
 
 const Nav = () => {
   const  [topics, setTopics] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sortByVisible, setSortByVisible] = useState(false);
+  const [order, toggleOrder] = useState("desc");
   const [searchParams, setSearchParams] = useSearchParams();
   const topic = searchParams.get("topic");
 
@@ -19,27 +22,41 @@ const Nav = () => {
     })
   }, [])
 
+  const handleSortByMouseEnter = () => {
+    setSortByVisible(true);
+  };
+
+  const handleSortByMouseLeave = () => {
+    setSortByVisible(false);
+  };
+
 
   return (
     <nav className="Nav">
       <ul >
           <Link to="/"><p className="nav-link"><strong>home</strong></p></Link>
-        {topics.map(({ slug }) => {
-          if (loading) {
-            return(
-              <li key="load-nav">
-                <p>Loading topic</p>
-              </li>
-            )
-          } else {
-          return(
-            <Link key={slug} to={`/articles/?topic=${slug}`}>
-              <p className={`nav-link ${topic === slug ? 'navbar-topic' : ""}`}><strong>{slug}</strong></p>
-            </Link>
-            )
-          }
-        }
-      )}
+            {topics.map(({ slug }) => {
+              if (loading) {
+                return(
+                  <li key="load-nav">
+                    <p>Loading topic</p>
+                  </li>
+                )
+              } else {
+              return(
+                <Link key={slug} to={`/articles/?topic=${slug}`}>
+                  <p className={`nav-link ${topic === slug ? 'navbar-topic' : ""}`}><strong>{slug}</strong></p>
+                </Link>
+                )
+              }
+            }
+          )}
+          <div className="hover-area" 
+              onMouseEnter={handleSortByMouseEnter}
+              onMouseLeave={handleSortByMouseLeave}>
+            <p>Sort options</p>
+          {sortByVisible ? <SortPanel order={order} toggleOrder={toggleOrder}/> : null}
+          </div>
       </ul>
     </nav>
   )
---
+++ src/components/SortPanel.jsx
@@ -0,0 +1,41 @@
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import { Link, useSearchParams } from 'react-router-dom'
+
+export default function SortPanel ({ order, toggleOrder }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const topic = searchParams.get("topic");
+
+  const path = `/articles/${topic ? `topic=${topic}` : ''}`
+
+  const handleChange = (event) => {
+    const newOrder = event.target.value
+    toggleOrder(newOrder) //#blueMonday#
+    updateParams('order', newOrder);
+  };
+
+  const updateParams = (param, value) => {
+    const newParams = new URLSearchParams(searchParams)
+    newParams.set(param, value);
+    setSearchParams(newParams);
+  }
+
+
+
+    return (<div className="sort-panel">
+        <button onClick={() => updateParams('sort_by', 'date')}><strong>Date</strong></button>
+        <button onClick={() => updateParams('sort_by', 'comment_count')}><strong>Comment count</strong></button>
+        <button onClick={() => updateParams('sort_by', 'votes')} ><strong>Votes</strong></button>
+          <RadioGroup
+              className="sort-panel-radio-switch"
+              defaultValue="desc"
+              name="radio-buttons-group"
+              onChange={handleChange}
+            >
+          <FormControlLabel value="asc" control={<Radio />} label="Ascending" />
+          <FormControlLabel value="desc" control={<Radio />} label="Descending" />
+        </RadioGroup>
+    </div>
+    )
+}
